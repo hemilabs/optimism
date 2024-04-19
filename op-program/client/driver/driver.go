ac59cc79aa5f6d49ea4cb686eb41568c7c02a713
ClaytonNorthey92 <clayton.northey@gmail.com>
2024-04-19 14:32:56 -0400

    signature

--- op-program/client/driver/driver.go
+++ op-program/client/driver/driver.go
@@ -41,7 +41,7 @@ type Driver struct {
 }
 
 func NewDriver(logger log.Logger, cfg *rollup.Config, l1Source derive.L1Fetcher, l1BlobsSource derive.L1BlobsFetcher, l2Source L2Source, targetBlockNum uint64, bssClient client.BssClient) *Driver {
-	engine := derive.NewEngineController(l2Source, logger, metrics.NoopMetrics, cfg, sync.CLSync)
+	engine := derive.NewEngineController(l2Source, logger, metrics.NoopMetrics, cfg, sync.CLSync, bssClient)
 	pipeline := derive.NewDerivationPipeline(logger, cfg, l1Source, l1BlobsSource, plasma.Disabled, l2Source, engine, metrics.NoopMetrics, &sync.Config{}, safedb.Disabled, bssClient)
 	pipeline.Reset()
 	return &Driver{
