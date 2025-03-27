4422c8ceb164eab15da8b1f5f861143ae9154f63
Max Sanchez <max@bloq.com>
2025-03-27 04:53:10 -0400

    Only set snapStarted = true when InsertUnsafePayload is successful

--- op-node/rollup/driver/state.go
+++ op-node/rollup/driver/state.go
@@ -263,11 +263,11 @@ func (s *Driver) eventLoop() {
 					continue
 				}
 
-				s.snapStarted = true
-
 				s.log.Info("Optimistically inserting unsafe L2 execution payload to drive EL sync", "id", envelope.ExecutionPayload.ID())
 				if err := s.Engine.InsertUnsafePayload(s.driverCtx, envelope, ref); err != nil {
 					s.log.Warn("Failed to insert unsafe payload for EL sync", "id", envelope.ExecutionPayload.ID(), "err", err)
+				} else {
+					s.snapStarted = true
 				}
 			}
 		case newL1Head := <-s.l1HeadSig:
