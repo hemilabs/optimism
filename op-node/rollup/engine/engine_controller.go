83ee8592cb1e513caf92a5efff40f32f25de32de
ClaytonNorthey92 <clayton.northey@gmail.com>
2025-04-24 21:07:44 -0400

    debug logging

--- op-node/rollup/engine/engine_controller.go
+++ op-node/rollup/engine/engine_controller.go
@@ -160,7 +160,9 @@ func (e *EngineController) BackupUnsafeL2Head() eth.L2BlockRef {
 }
 
 func (e *EngineController) IsEngineSyncing() bool {
-	return e.syncStatus == syncStatusWillStartEL || e.syncStatus == syncStatusStartedEL || e.syncStatus == syncStatusFinishedELButNotFinalized
+	result := e.syncStatus == syncStatusWillStartEL || e.syncStatus == syncStatusStartedEL || e.syncStatus == syncStatusFinishedELButNotFinalized
+	e.log.Debug("sync status is %s, result is %t", e.syncStatus, result)
+	return result
 }
 
 // Setters
