25c16a62fb818b299ca4317345721314373523fa
ClaytonNorthey92 <clayton.northey@gmail.com>
2025-05-29 19:09:22 -0400

    try reversing byte order

--- op-node/rollup/sequencing/sequencer.go
+++ op-node/rollup/sequencing/sequencer.go
@@ -5,9 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"slices"
 	"sync/atomic"
 	"time"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/hemilabs/heminetwork/hemi"
 	"github.com/protolambda/ctxlock"
 
@@ -831,7 +833,13 @@ func (d *Sequencer) calculatePoPPayoutTx(ctx context.Context, newBlockHeight uin
 		EPHash:             payoutBlock.Hash[:],
 	}
 
-	payoutL2KeystoneAbrevHash := hemi.L2KeystoneAbbreviate(*l2PayoutKeystone).Hash()
+	payoutL2KeystoneAbrevHashB := hemi.L2KeystoneAbbreviate(*l2PayoutKeystone).Hash().CloneBytes()
+	slices.Reverse(payoutL2KeystoneAbrevHashB)
+
+	payoutL2KeystoneAbrevHash, err := chainhash.NewHash(payoutL2KeystoneAbrevHashB)
+	if err != nil {
+		return nil, err
+	}
 
 	d.log.Info("querying for keystone", "L1BlockNumber", l2PayoutKeystone.L1BlockNumber,
 		"L2BlockNumber", l2PayoutKeystone.L2BlockNumber, "ParentEPHash", fmt.Sprintf("%x", l2PayoutKeystone.ParentEPHash),
