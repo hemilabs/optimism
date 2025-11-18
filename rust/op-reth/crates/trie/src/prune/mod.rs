0655642995ae74e59ad82d03cb86e71753861e8e
Sadiqur Rahman <sadiqurr8@gmail.com>
2025-11-18 21:45:31 +0600

    feat: Implemented `OpProofStoragePruner` (op-rs/op-reth#360)
    Co-authored-by: Emilia Hane <elsaemiliaevahane@gmail.com>

--- rust/op-reth/crates/trie/src/prune/mod.rs
+++ rust/op-reth/crates/trie/src/prune/mod.rs
@@ -3,9 +3,3 @@ pub use error::{OpProofStoragePrunerResult, PrunerError, PrunerOutput};
 
 mod pruner;
 pub use pruner::OpProofStoragePruner;
-
-#[cfg(feature = "metrics")]
-mod metrics;
-
-mod task;
-pub use task::OpProofStoragePrunerTask;
