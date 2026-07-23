7b51799ea2fafaa9ec3eb79f9915aedb338b5f19
Adrian Sutton <adrian@oplabs.co>
2026-07-23 10:21:22 +1000

    op-fetcher: Support super-root game types (#21928)

--- op-chain-ops/addresses/contracts.go
+++ op-chain-ops/addresses/contracts.go
@@ -80,6 +80,8 @@ type OpChainFaultProofsContracts struct {
 	FaultDisputeGameImpl               common.Address
 	FaultDisputeGameCannonKonaImpl     common.Address
 	PermissionedDisputeGameImpl        common.Address
+	SuperFaultDisputeGameImpl          common.Address
+	SuperPermissionedDisputeGameImpl   common.Address
 	DelayedWethPermissionedGameProxy   common.Address
 	DelayedWethPermissionlessGameProxy common.Address
 }
